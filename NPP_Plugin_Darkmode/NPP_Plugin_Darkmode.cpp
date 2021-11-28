54c9f4229de6dee8fdad931fc93e0a1d0a41d465
shriprem <shriprem@gmail.com>
2021-11-28 18:33:34 -0500

    Add project files.

---
+++ NPP_Plugin_Darkmode/NPP_Plugin_Darkmode.cpp
@@ -0,0 +1,42 @@
+#include "NPP_Plugin_Darkmode.h"
+
+void NPPDM_InitDarkMode(const HWND _nppHandle) {
+   nppHandle = _nppHandle;
+   return NppDarkMode::initDarkMode();
+}
+
+bool NPPDM_IsEnabled() {
+   return NppDarkMode::isEnabled();
+}
+
+bool NPPDM_IsToolBarFilled() {
+   return NppDarkMode::isToolBarFilled();
+}
+
+void NPPDM_AutoSubclassAndThemeChildControls(HWND hwndParent, bool subclass, bool theme) {
+   return NppDarkMode::autoSubclassAndThemeChildControls(hwndParent, subclass, theme);
+}
+
+void NPPDM_SetDarkTitleBar(HWND hwnd) {
+   return NppDarkMode::setDarkTitleBar(hwnd);
+}
+
+LRESULT NPPDM_OnCtlColor(HDC hdc) {
+   return NppDarkMode::onCtlColor(hdc);
+}
+
+LRESULT NPPDM_OnCtlColorSofter(HDC hdc) {
+   return NppDarkMode::onCtlColorSofter(hdc);
+}
+
+LRESULT NPPDM_OnCtlColorDarker(HDC hdc) {
+   return NppDarkMode::onCtlColorDarker(hdc);
+}
+
+LRESULT NPPDM_OnCtlColorError(HDC hdc) {
+   return NppDarkMode::onCtlColorError(hdc);
+}
+
+LRESULT NPPDM_OnCtlColorSysLink(HDC hdc) {
+   return NppDarkMode::onCtlColorSysLink(hdc);
+}
---
+++ NPP_Plugin_Darkmode/Utils.cpp
@@ -0,0 +1,48 @@
+#include "Utils.h"
+
+extern HWND nppHandle;
+
+int Utils::nppMessage(UINT messageID, WPARAM wparam, LPARAM lparam) {
+   return static_cast<int>(SendMessage(nppHandle, messageID, wparam, lparam));
+}
+
+string Utils::WideToNarrow(const wstring& wStr) {
+   return std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(wStr);
+}
+
+COLORREF Utils::intToRGB(int color) {
+   return RGB(GetRValue(color), GetGValue(color), GetBValue(color));
+}
+
+int Utils::scaleDPIX(int x) {
+   HDC hdc = GetDC(NULL);
+   if (!hdc) return 0;
+
+   int scaleX{ MulDiv(x, GetDeviceCaps(hdc, LOGPIXELSX), 96) };
+   ReleaseDC(NULL, hdc);
+   return scaleX;
+}
+
+int Utils::scaleDPIY(int y) {
+   HDC hdc = GetDC(NULL);
+   if (!hdc) return 0;
+
+   int scaleY{ MulDiv(y, GetDeviceCaps(hdc, LOGPIXELSY), 96) };
+   ReleaseDC(NULL, hdc);
+   return scaleY;
+}
+
+wstring Utils::getKnownFolderPath(REFKNOWNFOLDERID folderID) {
+   PWSTR path;
+
+   const HRESULT ret = SHGetKnownFolderPath(folderID, KF_FLAG_DEFAULT, NULL, &path);
+   if (ret != S_OK) return L"";
+
+   wstring sFolderPath{ path };
+   CoTaskMemFree(path);
+   return sFolderPath;
+}
+
+bool Utils::checkBaseOS(winVer os) {
+   return (nppMessage(NPPM_GETWINDOWSVERSION, NULL, NULL) >= os);
+}
